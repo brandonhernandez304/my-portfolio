43ddbfb9886810f4ecfd31909ea4610beff81fab
Brandon Hernandez <bhernandez30497@gmail.com>
2022-11-06 19:51:56 -0500

    adds alts to each img

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,13 +126,13 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
-              <Image src={proj1} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout="responsive"/>
+              <Image src={proj1} alt="project" classNme='rounded-lg object-cover' width={100} height={100} layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={proj3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout="responsive"/>
+              <Image src={proj3} alt="project" className='rounded-lg object-cover' width={100} height={100} layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={proj4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout="responsive"/>
+              <Image src={proj4} alt="project" className='rounded-lg object-cover' width={100} height={100} layout="responsive"/>
             </div>
           </div>
         </section>
